d668c6a7c40eb6628716cdeff3ddff35e9e9b8c1
tkmaher <tkmaher@umich.edu>
2025-11-16 12:46:59 -0600

    barred

--- src/main.tsx
+++ src/main.tsx
@@ -31,6 +31,11 @@ export function Lefthalf() {
         {title: "The Hunchback Variations", year: "2005", link: "https://vimeo.com/299786349"},
     ];
 
+    const radioplays: {title: string, year: string, link: string}[] = [
+        {title: "It Is Almost Always Upon Us", year: "2021", link: "https://drive.google.com/file/d/1NDsqEnqQYzp4zpGbWhvzPG0_Pm6bIS3l/view"},
+        {title: "The Cabinet", year: "2021", link: "https://www.dropbox.com/scl/fi/5qv0n97ryq1clcxmjpo2n/THE-CABINET-With-Frank-Intro-STEREO.mp3?rlkey=zwmmnfxc3rpb1akpujsxtag79&e=2&dl=0"},
+    ];
+
     return (
         <div className="left-half">
             <div className="content-box">
@@ -41,12 +46,15 @@ export function Lefthalf() {
                     <span className="separator">-</span>
                     <a href="micklemaher_cv.pdf" target="_blank">CV</a>
                     <span className="separator">-</span>
-                    <a style={{textDecoration: (section == "books" ? "underline" : "auto")}} onClick={() => setsection("books")}>Books/Performance Rights</a>
+                    <a style={{textDecoration: (section == "books" ? "underline" : "auto")}} onClick={() => setsection("books")}>Books</a>
                     <span className="separator">-</span>
-                    <a style={{textDecoration: (section == "videos" ? "underline" : "auto")}} onClick={() => setsection("videos")}>Video</a>
+                    <a style={{textDecoration: (section == "videos" ? "underline" : "auto")}} onClick={() => setsection("videos")}>Videos</a>
+                    <span className="separator">-</span>
+                    <a style={{textDecoration: (section == "radioplays" ? "underline" : "auto")}} onClick={() => setsection("radioplays")}>Radio Plays</a>
                     <span className="separator">-</span>
                     <a style={{textDecoration: (section == "contact" ? "underline" : "auto")}} onClick={() => setsection("contact")}>Contact</a>
-
+                    <span className="separator">-</span>
+                    <a href="https://sixplaysmicklemaher.com/rights-inquiries/" target="_blank">Performance Rights</a>
                 </div>
                 {section == "about" && <ReactMarkdown>Called one of the most original voices in American theater today, Chicago-based playwright Mickle Maher has been produced Off-Broadway (Barrow Street, 59 E 59), and by numerous theaters around the country and abroad. Plays include *It is Magic*; *There is a Happiness That Morning Is*; *Song About Himself*; *The Hunchback Variations; Spirits to Enforce;* book and lyrics for the musical *Small Ball*; *An Apology for the Course and Outcome of Certain Events Delivered by Doctor John Faustus on This His Final Evening; The Strangerer; Jim Lehrer and the Theater and Its Double and Jim Lehrer’s Double; Cyrano* (translator); *The Cabinet; The Pine*; *An Actor Prepares* (an adaptation of Stanislavsky's seminal book), and the award-winning adaptation of Jason Lutes’ graphic novel *Berlin.* He is a co-founder of Chicago’s Theater Oobleck, and has taught playwriting and related subjects at numerous institutions, including the University of Chicago, Northwestern, Columbia College, and the University of Houston. He is published by Agate Publishing. </ReactMarkdown>}
                 {section == "contact" && <div className="div-link">
@@ -57,6 +65,9 @@ export function Lefthalf() {
                     return <div className="div-link" key={index}><Link target="_blank" href={book[1]}>{book[0]}</Link><br/></div>
                 })}
                 {section == "videos" && plays.map((play, index) => {
+                    return <div className="div-link" key={index}><Link target="_blank" href={play.link}>{play.title} ({play.year})</Link><br/></div>
+                })}
+                {section == "radioplays" && radioplays.map((play, index) => {
                     return <div className="div-link" key={index}><Link target="_blank" href={play.link}>{play.title} ({play.year})</Link><br/></div>
                 })}
                 
