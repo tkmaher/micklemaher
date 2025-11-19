07b31a5032c841a6727d48f2cb0fbe9de4361047
tkmaher <tkmaher@umich.edu>
2025-11-19 12:11:14 -0600

    rebuilt

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { Body } from "@/src/main";
 
+// Written by Tomasz Maher, 2025.
+// https://otherseas1.com
 
 async function Home({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
